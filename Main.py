3bb2a5bbb0355180a60d9f7e41015909fe399be4
blanca890 <202310839@fit.edu.ph>
2025-03-15 23:01:35 +0800

    -add sample code

--- Main.py
+++ Main.py
@@ -5,3 +5,24 @@
 # Antonio Iii Guay
 # Josh Gabriel Bautista
 # Title: FoodLick
+
+
+
+
+while True:
+    print("Welcome to FoodLick")
+    print("1. Register")
+    print("2. Login")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        import Register
+    elif choice == "2":
+        import Login
+    elif choice == "3":
+        break
+    else:
+        print("Invalid choice")
+        print("Please try again")
+        print("")
